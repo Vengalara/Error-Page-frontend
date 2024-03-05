2d0d22069376dd57d9d278d6b422b9a478dc159f
Vengalara <95282202+Vengalara@users.noreply.github.com>
2024-03-05 13:39:15 -0500

    first commit

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+include("includes/header.php"); 
+
+?>
+
+<div class="container my-5">
+
+
+     <div class="col-md-8 mx-auto">
+           <h1 class="mb-3">Not Found</h1>
+           <p class="text-muted lead mb-3">What you are looking for doesn't exist.</p>
+        <a href="index.php" class="btn btn-success mb-3p">Back to Home</a>
+     </div>
+
+</div>
+
+
+<?php
+
+
+include 'includes/footer.php';
+
+?>
